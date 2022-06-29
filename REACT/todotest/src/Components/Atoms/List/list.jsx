37a65b323770037ae09ba3ba7a72c5914b9e010b
Benjamin Gillard <gillard.benjamin59@orange.fr>
2022-06-29 16:57:11 +0100

    ok

--- REACT/todotest/src/Components/Atoms/List/list.jsx
+++ REACT/todotest/src/Components/Atoms/List/list.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
-import Todo from "../Todo/todo";
+import { useState } from 'react'
+import Todo from '../Todo/todo'
 
-const List = ({title, id, deleteList}) => {
-
-    const [value,setValue] = useState("")
-    const [todos,setTodos] = useState([])
+const List = ({ title, id, deleteList }) => {
+    const [value, setValue] = useState('')
+    const [todos, setTodos] = useState([])
     console.log(todos)
 
-
-
     const handleClick = () => {
-        if(value) {
-            setTodos((prevState) => [...prevState, { id:Math.floor(Math.random()*1000) , title: value}])
-            setValue("")
+        if (value) {
+            setTodos((prevState) => [
+                ...prevState,
+                { id: Math.floor(Math.random() * 1000), title: value },
+            ])
+            setValue('')
         }
     }
 
@@ -23,36 +23,44 @@ const List = ({title, id, deleteList}) => {
     const deleteTodo = (todoID) => {
         let filtered = todos.filter((del) => del.id !== todoID)
         setTodos(filtered)
+        console.log('filter' + filtered)
     }
 
     return (
-        <div style={{border:"2px solid red", margin:10}}>
-        {title}
-        <input 
-            value={value}
-            placeholder="Créer une todo"
-            onChange={(e) => handleChange(e)}
+        <div style={{ border: '2px solid red', margin: 10 }}>
+            {title}
+            <input
+                value={value}
+                placeholder="Créer une todo"
+                onChange={(e) => handleChange(e)}
             />
 
-        <button onClick={() => {handleClick()}}>Crée todo</button>
-        
+            <button
+                onClick={() => {
+                    handleClick()
+                }}
+            >
+                Crée todo
+            </button>
 
-        {todos
-        .map((todo, index) => 
-        <Todo 
-            key={index}
-            title={todo.title}
-            id={todo.id}
-            deleteTodo={deleteTodo}/>
-        )}
+            {todos.map((todo, index) => (
+                <Todo
+                    key={index}
+                    title={todo.title}
+                    id={todo.id}
+                    deleteTodo={deleteTodo}
+                />
+            ))}
 
-        <button onClick={() => {deleteList(id)}}>Suppression Liste</button>
-
-
-        </div >
-        
+            <button
+                onClick={() => {
+                    deleteList(id)
+                }}
+            >
+                Suppression Liste
+            </button>
+        </div>
     )
-
 }
 
-export default List;
+export default List
--- REACT/todotest/src/Components/Molecules/listContainer.jsx
+++ REACT/todotest/src/Components/Molecules/listContainer.jsx
@@ -1,19 +1,19 @@
-import List from "../Atoms/List/list";
-import { useState } from "react";
+import List from '../Atoms/List/list'
+import { useState } from 'react'
 
 const ListContainer = () => {
-
-    const [value,setValue] = useState("")
-    const [lists,setLists] = useState([])
+    const [value, setValue] = useState('')
+    const [lists, setLists] = useState([])
     console.log(lists)
 
-
     const handleClick = () => {
-        if(value) {
-            setLists((prevState) => [...prevState, {id: Math.floor(Math.random()*1000), title: value}])
-            setValue("")
+        if (value) {
+            setLists((prevState) => [
+                ...prevState,
+                { id: Math.floor(Math.random() * 1000), title: value },
+            ])
+            setValue('')
         }
-        
     }
 
     const handleChange = (e) => {
@@ -23,34 +23,35 @@ const ListContainer = () => {
     const deleteList = (listID) => {
         let filtered = lists.filter((del) => del.id !== listID)
         setLists(filtered)
-
+        console.log(filtered)
     }
 
     return (
         <>
-        <input 
-            value={value} 
-            placeholder="Créer la Liste"
-            onChange={(e) => handleChange(e)}
-        />
+            <input
+                value={value}
+                placeholder="Créer la Liste"
+                onChange={(e) => handleChange(e)}
+            />
 
-        <button onClick={() => {handleClick()}}>Ajouter liste</button>
-    
-        {lists
-        .map ((list, index) => 
-        <List 
-            key={index}
-            title={list.title}
-            deleteList={deleteList}
-            id={list.id}
-        />
+            <button
+                onClick={() => {
+                    handleClick()
+                }}
+            >
+                Ajouter liste
+            </button>
 
-        )}
-        
-        
+            {lists.map((list, index) => (
+                <List
+                    key={index}
+                    title={list.title}
+                    deleteList={deleteList}
+                    id={list.id}
+                />
+            ))}
         </>
-        
     )
 }
 
-export default ListContainer;
+export default ListContainer
