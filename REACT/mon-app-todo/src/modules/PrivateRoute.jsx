723e54830665cb7d1596e12c5f6b8661e66db398
Benjamin Gillard <gillard.benjamin59@orange.fr>
2022-07-04 09:09:27 +0100

    create-react-app + create file

--- REACT/mon-app-todo/src/modules/PrivateRoute.jsx
+++ REACT/mon-app-todo/src/modules/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoute = () => {
 
-    const isLogin = false
+    const isLogin = true
 
     return( isLogin? <Outlet/> : <Navigate to='/login' />)
 }
